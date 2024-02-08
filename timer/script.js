ad44195bf8e293c1b531939f11863d8f4bcbe34b
v-rymashevsky <rymashevskiy.v@gmail.com>
2024-02-08 17:02:00 +0400

    added pause timer function

--- timer/script.js
+++ timer/script.js
@@ -9,22 +9,30 @@ const pauseButton = document.querySelector('.button-pause');
 startButton.addEventListener('click', startTimer)
 pauseButton.addEventListener('click', pauseTimer)
 
+// function reenableBtn() { 
+//     startButton.disabled = false; 
+//     pauseButton.disabled = true; 
+
+// }
 
 //functions
 
+let testID2;
+let count = 0;
+
 function startTimer() { 
-    time.innerText = '00 sec.';
-    let test = 0;
-    setInterval(() => {
-    test++;
-    time.innerText = `${test.toString().padStart(2, '0')} sec.`;
+    // time.innerText = '00 sec.';
+    testID2 = setInterval(() => {
+    count++;
+    time.innerText = `${count.toString().padStart(2, '0')} sec.`;
     }, 1000)
+    startButton.disabled = true; 
 }
 
+function pauseTimer () { 
+    clearInterval(testID2)
+    // testID2 = null;
+    startButton.disabled = false; 
 
-function pauseTimer() {
-    time.innerText = 'timer paused!'
+
 }
-
-
-
